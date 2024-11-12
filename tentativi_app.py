cfc8e62b579bf719ccf2ba73e00b3d370a43ae14
Chiara Valentini <chiara.valentini.5@studenti.unipd.it>
2024-11-12 14:04:28 +0100

    TIDYING
    Ho cercato di pulire i dati prendendo i valori più recenti
    
    Problema:
    descrizioni e valutazioni non sono state assegnate ai libri corretti

--- tentativi_app.py
+++ tentativi_app.py
@@ -2,8 +2,9 @@ import streamlit as st
 import polars as pl
 import altair as alt  # mi serve per avere grafici ordinati secondo ciò che voglio io, non secondo l'ordine alfabetico delle x
 
-data = pl.read_csv("sf_books_tidy.csv")
+data = pl.read_csv("sf_books_tidy_updated.csv")
 
+# Titolo
 st.write("""
 # Libri di Fantascienza
 """)
@@ -28,18 +29,57 @@ bar_valutazione_autore = (
     .sort("Rating_score", descending=True)
 )
 
+# ha qualcosa che non va... es: per Asimov, Nightfall ha barra più lunga ma punteggio minore di altre barre più corte
 st.bar_chart(
      bar_valutazione_autore,
      x = "Book_Title",
-     x_label = "Titolo",
+     x_label = "Valutazione",
      y="Rating_score",
-     y_label = "Valutazione",
+     y_label = "Titolo",
     horizontal=True
     )
 
 
+# Autori più prolifici (metto i primi 25)
 
+st.write("""
+## Quali sono gli autori più prolifici?
+""")
 
+scatter_autori_nlibri = (
+    data
+    .group_by(pl.col("Author_Name"))
+    .agg(
+        pl.count("Book_Title")
+         .alias("Book_Number")
+    ).sort("Book_Number", descending = True)
+)
+
+# è un po' brutto, come migliorare?
+st.scatter_chart(
+    scatter_autori_nlibri.head(25),
+    x = "Author_Name",
+    x_label = "Autore",
+    y = "Book_Number",
+    y_label = "Numero di libri",
+    size = "Book_Number",
+    height = 500
+)
+
+# o meglio?
+st.bar_chart(
+    scatter_autori_nlibri.head(15),
+    x = "Author_Name",
+    x_label = "Autore",
+    y = "Book_Number",
+    y_label = "Numero di libri",
+    horizontal = True
+)
+
+# Le valutazioni sono influenzate dall'anno di uscita?
+# Il numero di valutazioni è influenzato dall'anno di uscita?
+# Qual è il genere più amato? (con migliori valutazioni)
+# L'anno di pubblicazione influenza il genere di un libro? (anni in classi ampie 10 o 20 vs numero di libri per ogni genere)
 
 
 
