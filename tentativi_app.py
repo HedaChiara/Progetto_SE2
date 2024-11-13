8ab96ec9b591180f5caf5cb86512c0d406a4e2d6
Chiara Valentini <chiara.valentini.5@studenti.unipd.it>
2024-11-13 15:02:44 +0100

    FINAL DATASET E PROVE APP
    - Ho pulito ufficialmente i dati
    - Ho iniziato a provare a visualizzare gli autori più prolifici secondo i generi selezionati

--- tentativi_app.py
+++ tentativi_app.py
@@ -8,27 +8,27 @@ st.write("""
 # Libri di Fantascienza
 """)
 
-# libri considerati migliori, per autore
+
+#### Libri considerati migliori, per autore ####
 st.write("""
 ## Quali sono le valutazioni dei libri di un certo autore?
 """)
 autori = data.select("Author_Name").unique().sort("Author_Name")
 
+# selezione autore da parte dell'utente
 autore_selezionato = st.selectbox(
     "Seleziona un autore",
     autori,
     index = None,
     placeholder = ""
 )
-
 bar_valutazione_autore = (
     data
     .filter(pl.col("Author_Name") == autore_selezionato)
     .select(pl.col(["Book_Title", "Rating_score"]))
     .sort("Rating_score", descending=True)
 )
-
-# ha qualcosa che non va... es: per Asimov, Nightfall ha barra più lunga ma punteggio minore di altre barre più corte
+# grafico a barre (si può ordinare?)
 st.bar_chart(
      bar_valutazione_autore,
      x = "Book_Title",
@@ -39,13 +39,25 @@ st.bar_chart(
     )
 
 
-# Autori più prolifici (metto i primi 25)
 
+#### Autori più prolifici, per genere (metto i primi 25) ####
 st.write("""
 ## Quali sono gli autori più prolifici?
 """)
 
-scatter_autori_nlibri = (
+generi = ["Alieni", "Alternate History", "Universo alternativo", "Apocalittico", "Cyberpunk", 
+          "Distopia", "Fantascienza Hard", "Militare", "Robot", "Space Opera", "Steampunk", "Viaggi nel Tempo"]
+# selezione dei generi da parte dell'utente
+generi_selezionati = st.multiselect(
+    "Seleziona uno o più generi",
+    generi,
+    placeholder = "",
+    # di default, mostra gli autori più prolifici in generale (per tutti i generi)
+    default = generi
+)
+
+# dataframe con colonne Autore - Numero di libri scritti - Generi
+autori_nlibri = (
     data
     .group_by(pl.col("Author_Name"))
     .agg(
@@ -54,26 +66,22 @@ scatter_autori_nlibri = (
     ).sort("Book_Number", descending = True)
 )
 
-# è un po' brutto, come migliorare?
-st.scatter_chart(
-    scatter_autori_nlibri.head(25),
-    x = "Author_Name",
-    x_label = "Autore",
-    y = "Book_Number",
-    y_label = "Numero di libri",
-    size = "Book_Number",
-    height = 500
-)
+autori_nlibri.write_csv("Book_Count_by_Author.csv")
 
-# o meglio?
+
+
+# grafico a barre
 st.bar_chart(
-    scatter_autori_nlibri.head(15),
+    autori_nlibri.head(15),
     x = "Author_Name",
     x_label = "Autore",
     y = "Book_Number",
     y_label = "Numero di libri",
     horizontal = True
 )
+# Volendo posso importare immagine presa da grafico R ggplot... e in appendice metto come ho fatto a farlo
+
+
 
 # Le valutazioni sono influenzate dall'anno di uscita? plot year vs rating score
 # Il numero di valutazioni è influenzato dall'anno di uscita? 
