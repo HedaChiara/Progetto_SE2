d2e9150d94f7241bf34ad99f3429e5c72858127a
Chiara Valentini <chiara.valentini.5@studenti.unipd.it>
2024-11-21 17:54:43 +0100

    ADD VALUTAZIONE LIBRI PIE
    - finire di mettere il testo

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ st.write("""
 st.write("""
 ### Quali sono le valutazioni dei libri di un certo autore?
          
-Il seguente grafico consente di selezionare un autore e visualizzare le valutazioni di ogni suo libro
+Il seguente grafico consente di selezionare un autore e visualizzare le valutazioni di ogni suo libro.  
+Le valutazioni vanno da un minimo di 0 ad un massimo di 5
 """)
 
 autori = data.select("Author_Name").unique().sort("Author_Name")
@@ -111,6 +112,84 @@ if not check_valutazioni1:
     st.altair_chart(bar_rating_genere, use_container_width=True)
 else:
     st.altair_chart(bar_rating_genere_famosi, use_container_width=True)
+
+
+#### Grafico a torta delle valutazioni (tra 1 e 3, tra 3 e 4, tra 4 e 5) -> gli utenti di Goodreads sono generosi? ####
+st.write('''
+### Che valutazioni vengono date ai libri?
+''')
+# raggruppo le valutazioni in 4 classi
+classi_valutazioni = {"Scadente" : [0, 3.5], "Mediocre" : [3.5, 4], "Buono" : [4, 4.5], "Eccellente" : [4.5, 5]}
+# aggiungo una colonna in cui ho la classe di valutazione a cui appartiene il libro
+data = data.with_columns(
+    Rating_score_class =
+    pl.when(pl.col("Rating_score") >= classi_valutazioni["Scadente"][0], pl.col("Rating_score") < classi_valutazioni["Scadente"][1] ).then(pl.lit("Scadente (tra 0 e 3.5)"))
+    .when(pl.col("Rating_score") >= classi_valutazioni["Mediocre"][0], pl.col("Rating_score") < classi_valutazioni["Mediocre"][1] ).then(pl.lit("Mediocre (tra 3.5 e 4)"))
+    .when(pl.col("Rating_score") >= classi_valutazioni["Buono"][0], pl.col("Rating_score") < classi_valutazioni["Buono"][1] ).then(pl.lit("Buono (tra 4 e 4.5)"))
+    .when(pl.col("Rating_score") >= classi_valutazioni["Eccellente"][0], pl.col("Rating_score") < classi_valutazioni["Eccellente"][1] ).then(pl.lit("Eccellente (tra 4.5 e 5)"))
+    .alias("Rating_score_class")
+)
+# st.write(media_valutazioni = data.select("Rating_score").mean())
+
+check_valutazioni2 = st.checkbox(
+    f"Mostra solo le valutazioni dei libri con più di mezzo milione di valutazioni",
+    value = False
+)
+
+# COME CAMBIO L'ORDINE DELLE CATEGORIE NELLA LEGENDA?
+
+# grafico a torta delle valutazioni
+pie_valutazioni = (
+    alt.Chart(data.drop_nulls("Rating_score_class"))
+    .mark_arc(radius=70, radius2=130, cornerRadius=15)
+    .encode(
+        alt.Theta("Rating_score_class", aggregate="count"),
+        alt.Color("Rating_score_class", sort=["Scadente", "Mediocre", "Buono", "Eccellente"]).scale(scheme="category10").legend(title="Valutazione"),
+        
+    )
+)
+# percentuali per ogni classe
+percentuali_valutazioni = data.group_by("Rating_score_class").agg()
+pie_valutazioni_text = (
+    alt.Chart(percentuali_valutazioni)
+    .mark_text(radius=120)
+    .encode(
+        alt.Text("", aggregate="count"),
+        alt.Theta("pop", aggregate="sum"),
+        alt.Color("continent")
+    )
+
+)
+
+# solo per i libri più famosi
+pie_valutazioni_famosi =(
+    alt.Chart(data.drop_nulls("Rating_score_class").filter(pl.col("Rating_votes") >= 500000))
+    .mark_arc(radius=70, radius2=130, cornerRadius=15)
+    .encode(
+        alt.Theta("Rating_score_class", aggregate="count"),
+        alt.Color("Rating_score_class", sort=["Scadente", "Mediocre", "Buono", "Eccellente"]).scale(scheme="category10").legend(title="Valutazione", ),
+        
+    )
+)
+# testo con le percentuali
+pie_valutazioni_famosi_text = (
+
+)
+
+if check_valutazioni2:
+    st.altair_chart(pie_valutazioni_famosi + pie_valutazioni_text, use_container_width=True)
+else:
+    st.altair_chart(pie_valutazioni + pie_valutazioni_famosi_text, use_container_width=True)
+
+st.write('''
+Gli utenti di Goodreads hanno valutato come mediocre o buona la qualità della maggior parte dei libri di fantascienza nel catalogo.  
+Guardando solamente le valutazioni dei libri più famosi, scompare la categoria "Scadente" ed aumenta invece la proporzione di libri valutati come buoni ed eccellenti.  
+''')
+
+
+
+
+
 
 
 # Qual è il genere più amato? (con migliori valutazioni) barplot genere vs rating score
@@ -226,8 +305,13 @@ anni = [range(1900, 1970), range(1971, 1980), range(1981, 1990), range(1991, 200
 
 
 #### RECOMMENDATION SYSTEM - INSERISCO UN LIBRO CHE MI PIACE E ME NE SUGGERISCE ALTRI (potrei inserire dei filtri)
+# Idee: 
+# - PRIMO APPROCCIO: dopo un opportuno preprocessing delle descrizioni (eliminazione delle stop word, tokenizzazione, stemming)
+# posso usare word2vec e poi trovo i libri più simili a quello dato usando una metrica di similarità tipo la cosine similarity o la distanza euclidea
+# - COSA PIU' STRUTTURATA: faccio clustering tenendo conto anche dei generi e dell'autore (se dello stesso autore, aumenta la similarità)
+# - MIGLIORAMENTI: ampliare le descrizioni esistenti aggiungendo altro
 
-#### SCRIVO UNA PAROLA E MI DICE QUALI LIBRI PARLANO DI QUESTA COSA (magari faccio vedere solo i migliori)
+#### SCRIVO UNA PAROLA (o una frase) E MI DICE QUALI LIBRI PARLANO DI QUESTA COSA (magari faccio vedere solo i migliori)
 
 
 
