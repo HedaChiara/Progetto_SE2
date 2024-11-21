5f95943d2d0d34f2bf95452929dca0cd38201ebb
Chiara Valentini <chiara.valentini.5@studenti.unipd.it>
2024-11-21 12:53:52 +0100

    WORKING ON STUFF

--- app.py
+++ app.py
@@ -15,15 +15,20 @@ st.write("""
 
 st.write(
 f"""
-Le seguenti analisi si basano sui dati su libri di fantascienza scaricabili [qui]({url}), opportunamente ripuliti.
-La procedura di preprocessing dei dati è documentata nell'Appendice A <- scoprire come mettere il riferimento
+Le seguenti analisi si basano su dati riguardanti libri di fantascienza estratti da [Goodreads]({"https://www.goodreads.com/"}) e scaricabili [qui]({url}), opportunamente preprocessati.
+La procedura di pulizia dei dati è documentata nell'Appendice A, in fondo alla pagina.  
+
  """
          )
 
+## ANALISI SUI RATING SCORE
+st.write("""
+## Analisi riguardanti le valutazioni dei libri
+""")
 
 #### Libri considerati migliori, per autore ####
 st.write("""
-## Quali sono le valutazioni dei libri di un certo autore?
+### Quali sono le valutazioni dei libri di un certo autore?
          
 Il seguente grafico consente di selezionare un autore e visualizzare le valutazioni di ogni suo libro
 """)
@@ -33,8 +38,9 @@ autori = data.select("Author_Name").unique().sort("Author_Name")
 autore_selezionato = st.selectbox(
     "Seleziona un autore",
     autori,
-    index = None,
-    placeholder = ""
+    index = 3021,
+    placeholder = "",
+
 )
 bar_valutazione_autore = (
     alt.Chart(data.filter(pl.col("Author_Name") == autore_selezionato).select(pl.col(["Book_Title", "Rating_score"])))
@@ -52,31 +58,37 @@ st.altair_chart(bar_valutazione_autore, use_container_width=True)
 
 #### Libri con valutazione migliore, per genere ####
 st.write("""
-## Quali sono i libri con valutazioni migliori di un dato genere?
+### Quali sono i libri con valutazioni migliori di un dato genere?
          
-Il seguente grafico consente di selezionare un sottogenere della fantascienza e visualizzare i libri meglio valutati.
+Il seguente grafico consente di selezionare un sottogenere della fantascienza e visualizzare i libri meglio valutati, con la possibilità
+di escludere quelli meno famosi (quelli con meno di 750000 valutazioni).
 """)
-
-# selezione del numero di libri da visualizzare da parte dell'utente
-n_libri = range(1, 101)
-n_libri_selezionato = st.select_slider("Quanti libri vorresti visualizzare?", n_libri, value=20)
 
 # selezione del genere da parte dell'utente
 generi = ["Alieni", "Alternate History", "Universo Alternativo", "Apocalittico", "Cyberpunk", 
           "Distopia", "Fantascienza Hard", "Militare", "Robot", "Space Opera", "Steampunk", "Viaggi nel Tempo"]
+diz_generi_indicatrici = {
+    "Alieni" : "I_alien", "Alternate History" : "I_alt_hist", "Universo Alternativo" : "I_alt_uni", "Apocalittico" : "I_apo", 
+    "Cyberpunk" : "I_cpunk", "Distopia" : "I_dyst", "Fantascienza Hard" : "I_hard", "Militare" : "I_mil", "Robot" : "I_robots", 
+    "Space Opera" : "I_space", "Steampunk" : "I_steam", "Viaggi nel Tempo" : "I_ttravel"
+}
 genere_selezionato = st.selectbox(
     "Seleziona un genere",
     generi,
     index = 5,
     placeholder = "",
 )
-# grafici a barre dei libri migliori, in base al genere e al numero selezionati
-diz_generi_indicatrici = {
-    "Alieni" : "I_alien", "Alternate History" : "I_alt_hist", "Universo Alternativo" : "I_alt_uni", "Apocalittico" : "I_apo", 
-    "Cyberpunk" : "I_cpunk", "Distopia" : "I_dyst", "Fantascienza Hard" : "I_hard", "Militare" : "I_mil", "Robot" : "I_robot", 
-    "Space Opera" : "I_space", "Steampunk" : "I_steam", "Viaggi nel Tempo" : "I_ttravel"
-}
 
+# selezione del numero di libri da visualizzare da parte dell'utente
+n_libri = range(1, 201)
+n_libri_selezionato = st.select_slider("Quanti libri vorresti visualizzare, al massimo?", n_libri, value=20)
+
+check_valutazioni1 = st.checkbox(
+    f"Mostra solo i libri di questo sottogenere con più di 750000 valutazioni",
+    value = False
+)
+
+# grafici a barre dei libri migliori, in base al genere e al numero selezionati
 bar_rating_genere = (
     alt.Chart(data.filter(pl.col(diz_generi_indicatrici[genere_selezionato]) == 1).sort(pl.col("Rating_score"), descending=True).head(n_libri_selezionato))
     .mark_bar()
@@ -86,15 +98,34 @@ bar_rating_genere = (
     )
     )
 
-st.altair_chart(bar_rating_genere, use_container_width=True)
-# Upgrade:    
-# Farlo solo per i libri più famosi
+bar_rating_genere_famosi = (
+    alt.Chart(data.filter(pl.col(diz_generi_indicatrici[genere_selezionato]) == 1).filter(pl.col("Rating_votes") >= 750000).sort(pl.col("Rating_score"), descending=True).head(n_libri_selezionato))
+    .mark_bar()
+    .encode(
+        alt.Y("Book_Title", sort="-x", title="Titolo"),
+        alt.X("Rating_score", title="Valutazione")
+    )
+    )
+
+if not check_valutazioni1:
+    st.altair_chart(bar_rating_genere, use_container_width=True)
+else:
+    st.altair_chart(bar_rating_genere_famosi, use_container_width=True)
 
 
+# Qual è il genere più amato? (con migliori valutazioni) barplot genere vs rating score
+
+
+
+
+## ANALISI SUL NUMERO DI LIBRI SCRITTI NEGLI ANNI
+st.write("""
+## Analisi sul numero di libri scritti negli anni
+""")
 
 #### Autori più prolifici (primi 15) ####
 st.write("""
-## Quali sono gli autori più prolifici?
+### Quali sono gli autori più prolifici?
 """)
 # dataframe 
 autori_nlibri = pl.read_csv("Book_Count_by_Author.csv")
@@ -109,27 +140,32 @@ bar_autori_prolifici = (
 )
 st.altair_chart(bar_autori_prolifici, use_container_width=True)
 
-# Upgrade:
+# Upgrade: far vedere gli autori più prolifici per genere (è un casino raggruppare per genere, ho solo indicatrici)
+# dovrei guardare, per ogni indicatrice di genere, quali righe hanno 1 in quella colonna e tenere traccia dell'autore (unique)
+
 # st.write("""
 # ## Che genere di libri hanno scritto? E in che anni?
 # """)
 
-# Possibile upgrade: far vedere gli autori più prolifici per genere (è un casino raggruppare per genere, ho solo indicatrici)
-# dovrei guardare, per ogni indicatrice di genere, quali righe hanno 1 in quella colonna e tenere traccia dell'autore (unique)
+
 
 
 #### Anni in cui sono stati scritti più libri ####
 st.write("""
-## Quali sono gli anni in cui sono stati scritti più libri di fantascienza?
+### Quali sono gli anni in cui sono stati scritti più libri di fantascienza?
 Il grafico seguente mostra l'andamento del numero di libri di fantascienza scritti nel corso degli anni dal 1900 al 2020
 """)
 
-# checkbox per far decidere se visualizzare anche grafico per libri molto valutazioni
-# volendo posso mettere anche uno slidere per far decidere il numero di valutazioni
-check_valutazioni = st.checkbox(
-    "Visualizza l'andamento solo per libri con più di 750000 valutazioni",
+# checkbox per far decidere se visualizzare anche grafico per libri molto valutati
+# volendo posso mettere anche uno slider per far decidere il numero di valutazioni
+n_valutazioni = st.number_input(label="Inserisci qui il numero minimo di valutazioni che un libro deve avere per essere mostrato",min_value=0, max_value=5000000, label_visibility="visible")
+
+check_valutazioni2 = st.checkbox(
+    f"Visualizza anche l'andamento per i libri con più di {n_valutazioni} valutazioni",
     value = False
 )
+
+# LAYER ALTAIR ...
 
 book_count_year = pl.read_csv("book_count_per_year.csv")
 st.line_chart(
@@ -142,31 +178,33 @@ st.line_chart(
 
 famous_books = pl.read_csv("famous_books.csv")
 # posso farlo con Altair (aggiungere grafico a grafico già esistente)
-if check_valutazioni:
+if check_valutazioni2:
     st.line_chart(
         famous_books,
         x="Year_published",
         y="Book_Count"
 )
 
+# mettere anche checkbox per vedere solo andamento dei libri con recensioni > della media delle valutazioni
+
 st.write("""
-Si nota un andamento tendenzialmente crescente del numero di libri di fantascienza scritti negli anni, in particolare è evidente 
-una rapida ascesa negli anni seguenti il 2005 che culmina con un picco nel 2013.
-Goodreads è stato lanciato nel 2007, per cui una spiegazione plausibile del trend osservato è che a seguito della sua creazione 
-siano stati inseriti nel catalogo più libri poco famosi o di autori meno affermati rispetto a quanto fatto in passato. 
-Un'ulteriore indicazione di ciò è che il picco del 2013 coincide con l'anno di acquisto di Goodreads da parte di Amazon, 
-che aggiunge automaticamente i libri presenti nel proprio catalogo a quello di Goodreads. (?) 
-Magari invece sono stati fattori sociali e tecnologici, ma che coincidenza... 
-Si riscontra infine un notevole decremento del numero di libri scritti negli anni seguenti al 2013.
+Si nota un andamento tendenzialmente crescente del numero di libri di fantascienza scritti nel corso del tempo, in particolare è evidente 
+una rapida ascesa negli anni successivi al 2005 che culmina con un picco nel 2013.  
+Probabilmente questo rapido aumento è dovuto all'evoluzione e alla diffusione della tecnologia e al boom dell'editoria digitale che ha permesso a chiunque
+di scrivere e pubblicare libri molto più facilmente rispetto al passato.  
+E' curioso notare che Goodreads è stato lanciato nel 2007 e che Amazon, che aggiunge i libri presenti nel proprio catalogo
+a quello di Goodreads in modo automatico, l'ha acquistato proprio nel 2013, anno in cui si è registrato il picco di libri scritti.  
+Si riscontra infine un notevole decremento del numero di libri scritti dopo il 2013.
 """)
 
 
 st.write("""
-## Quanti libri di fantascienza appartenenti ai vari sottogeneri sono stati scritti negli anni?
+### Quanti libri di fantascienza appartenenti ai vari sottogeneri sono stati scritti negli anni?
 Il seguente grafico consente di selezionare uno o più sottogeneri e di visualizzare il numero di libri appartenenti
 a quei sottogeneri scritti negli anni
 """)
 
+# LAYERS 
 
 
 
@@ -180,18 +218,28 @@ anni = [range(1900, 1970), range(1971, 1980), range(1981, 1990), range(1991, 200
 
 
 
-
-# Volendo posso importare immagini prese da grafici R ggplot... e in appendice metto come ho fatto a farlo
-
-
-
 # Le valutazioni sono influenzate dall'anno di uscita? plot year vs rating score
 # Il numero di valutazioni è influenzato dall'anno di uscita? 
-# Qual è il genere più amato? (con migliori valutazioni) barplot genere vs rating score
 
 
 
 
+
+#### RECOMMENDATION SYSTEM - INSERISCO UN LIBRO CHE MI PIACE E ME NE SUGGERISCE ALTRI (potrei inserire dei filtri)
+
+#### SCRIVO UNA PAROLA E MI DICE QUALI LIBRI PARLANO DI QUESTA COSA (magari faccio vedere solo i migliori)
+
+
+
+# APPENDICE A - PREPROCESSING DEI DATI
+with st.expander(label = "Appendice A", expanded=False, icon=None):
+    st.write('''
+    Spiegazione della pulizia:  
+    Blah blah blah
+    ''')
+    # scrivere una breve spiegazione
+    # rimandare a Tidying.py
+    pass
 
 
 
